9293441a03e4b248bb1513c9e94657399d845241
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-09 01:43:07 -0500

    Multi unit test

--- test_main.py
+++ test_main.py
@@ -3,3 +3,15 @@ from main import suma
 
 def test_suma():
     assert suma(2, 2) == 4
+
+@pytest.mark.parametrize(
+    "input_a, input_b, expected",
+    [
+        (3,2,5),
+        (2,3,4),
+        (suma(3,2),5,10),
+        (3,suma(2,5),10),
+    ]
+)
+def test_suma_multi(input_a, input_b, expected):
+    assert suma(input_a, input_b) == expected
