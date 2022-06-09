8ad091d6ce8e970ecf9b96b66f92e79b825081e7
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-09 01:30:06 -0500

    Creating the add function

---
+++ main.py
@@ -0,0 +1,9 @@
+def suma(x, y):
+    """
+    Funcion que toma 2 argumentos y devuelve la suma de los mismos
+    Args:
+        x (int/float): Primer valor a sumar
+        y (int/float): Segundo valor a sumar
+    return: x+y
+    """
+    return x+y
