97a9804b45aa5779a79d9bfd1989310e5d0b8eca
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-09 02:26:42 -0500

    Adding a class for test

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ def write(fpath, data_in):
     """
     with open(fpath, "w") as file_in:
         file_in.write(data_in)
+
+class calculator:
+    """
+    Class calculator
+    """
+    def suma(x, y):
+        """
+        Funcion que toma 2 argumentos y devuelve la suma de los mismos
+        Args:
+            x (int/float): Primer valor a sumar
+            y (int/float): Segundo valor a sumar
+        return: x+y
+        """
+        return x+y
+    
